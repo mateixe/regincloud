23904acb82511da1cc057ceeeb00322fccf42ce7
mateixe <mateixe@gmail.com>
2017-10-20 15:39:37 -0200

    Add files via upload
    Final version of beta

--- RegInCloud/RegInCloud/RegInCloud.py
+++ RegInCloud/RegInCloud/RegInCloud.py
@@ -1,1 +1,15 @@
-print("hello isnt me that you looking for")
+import web
+
+urls = {
+    '/','index'
+}
+
+app = web.application(urls,globals())
+
+class index:
+    def GET(self):
+        greeting = "Hello World"
+        return greeting
+
+if __name__ == "_main__":
+    app.run()
